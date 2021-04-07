fbb400ddb684e8c26d8365a4e833be0f0082336f
Marton Barta <bbartam@gmail.com>
2021-04-07 12:17:43 +0200

    added logrus http logger

--- shorter/cmd/shorter/shorter.go
+++ shorter/cmd/shorter/shorter.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"github.com/bartam1/mobilfox/shorter/internal/handlers/httphandler"
+	"github.com/bartam1/mobilfox/shorter/pkg/logs/httplog"
 	echo "github.com/labstack/echo/v4"
-	//echomiddleware "github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(httplog.MiddlewareLogging)
+	e.HTTPErrorHandler = httplog.ErrorHandler
+
 	httphandler.RegisterHandlers(e, nil)
 	e.Start("0.0.0.0:7777")
 }
