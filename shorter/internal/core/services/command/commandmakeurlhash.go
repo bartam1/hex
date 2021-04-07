4febc99957ea50efa89f4e540f0edbe81faff81d
Marton Barta <bbartam@gmail.com>
2021-04-07 12:01:03 +0200

    mod: generate the hash, and return url-hash pair

--- shorter/internal/core/services/command/commandmakeurlhash.go
+++ shorter/internal/core/services/command/commandmakeurlhash.go
@@ -7,7 +7,7 @@ import (
 )
 
 type MakeUrlHashModel interface {
-	MakeUrlHash(ctx context.Context, mu domain.MakeUrlHash) (u domain.UrlHash, err error)
+	MakeUrlHash(ctx context.Context, mu domain.UrlHash) (u domain.UrlHash, err error)
 }
 
 type MakeUrlHash struct {
@@ -18,7 +18,7 @@ func NewMakeUrlHash(model MakeUrlHashModel) MakeUrlHash {
 	return MakeUrlHash{writeModel: model}
 }
 
-func (h MakeUrlHash) Do(ctx context.Context, u domain.MakeUrlHash) (user domain.UrlHash, err error) {
-	//TODO: generate hash inside core domain
-	return h.writeModel.MakeUrlHash(ctx, u)
+func (h MakeUrlHash) Do(ctx context.Context, u domain.MakeUrlHash) (urlh domain.UrlHash, err error) {
+	uh := domain.GenerateUrlHash(u.Url)
+	return h.writeModel.MakeUrlHash(ctx, uh)
 }
