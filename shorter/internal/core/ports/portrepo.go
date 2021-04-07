9e90e5644e80626c9bcb9f2d06c912a179a511e7
Marton Barta <bbartam@gmail.com>
2021-04-07 10:52:15 +0200

    added port repository

---
+++ shorter/internal/core/ports/portrepo.go
@@ -0,0 +1,14 @@
+package port
+
+import (
+	"context"
+
+	"github.com/bartam1/mobilfox/shorter/internal/core/domain"
+)
+
+type RepoIf interface {
+	GetUrlsWidthHash(ctx context.Context) (us []domain.UrlHash, err error)
+	GetUrl(ctx context.Context, url string) (u domain.UrlHash, err error)
+	MakeUrlHash(ctx context.Context, mu domain.MakeUrlHash) (u domain.UrlHash, err error)
+	DeleteUrl(ctx context.Context, hash string) error
+}
