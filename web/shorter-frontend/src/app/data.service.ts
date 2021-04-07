93f907cb928b0d89f1ab878c32d908e83c0bb566
Marton Barta <bbartam@gmail.com>
2021-04-08 01:27:47 +0200

    added data service to get data from backend

---
+++ web/shorter-frontend/src/app/data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private API_SERVER = "http://localhost:3000";
+
+  constructor(private httpClient: HttpClient) { }
+  public GetUrlsWidthHash(){
+    let url = this.API_SERVER + "/urls";
+    return this.httpClient.get(url);
+  }
+}
