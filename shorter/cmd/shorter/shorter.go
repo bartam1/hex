c15f01a7035589bf5aa5e80bdb7294a992ba13c3
Marton Barta <bbartam@gmail.com>
2021-04-07 14:12:49 +0200

    added repo, service, handler into main

--- shorter/cmd/shorter/shorter.go
+++ shorter/cmd/shorter/shorter.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	port "github.com/bartam1/mobilfox/shorter/internal/core/ports"
+	"github.com/bartam1/mobilfox/shorter/internal/core/services/command"
+	"github.com/bartam1/mobilfox/shorter/internal/core/services/query"
 	"github.com/bartam1/mobilfox/shorter/internal/handlers/httphandler"
+	"github.com/bartam1/mobilfox/shorter/internal/repositories/memrepo"
 	"github.com/bartam1/mobilfox/shorter/pkg/logs/extlog"
 	"github.com/bartam1/mobilfox/shorter/pkg/logs/httplog"
 	echo "github.com/labstack/echo/v4"
@@ -10,11 +14,25 @@ import (
 func main() {
 	extlog.Init()
 
+	repo, _ := memrepo.New()
+
+	s := port.Service{
+		Queries: port.Queries{
+			UrlsWidthHash: query.NewUrlsWidthHash(repo),
+			Url:           query.NewUrl(repo),
+		},
+		Commands: port.Commands{
+			MakeUrlHash: command.NewMakeUrlHash(repo),
+			DeleteUrl:   command.NewDeleteUrl(repo),
+		},
+	}
+	hndl := httphandler.New(s)
+
 	e := echo.New()
 
 	e.Use(httplog.MiddlewareLogging)
 	e.HTTPErrorHandler = httplog.ErrorHandler
 
-	httphandler.RegisterHandlers(e, nil)
+	httphandler.RegisterHandlers(e, hndl)
 	e.Start("0.0.0.0:7777")
 }
