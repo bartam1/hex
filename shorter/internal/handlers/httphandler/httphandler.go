6bc274191ad6562d33c622ab6d476045eb8528ff
Marton Barta <bbartam@gmail.com>
2021-04-07 14:01:11 +0200

    added http handler funcs

---
+++ shorter/internal/handlers/httphandler/httphandler.go
@@ -0,0 +1,49 @@
+package httphandler
+
+import (
+	"net/http"
+
+	"github.com/bartam1/mobilfox/shorter/internal/core/domain"
+	port "github.com/bartam1/mobilfox/shorter/internal/core/ports"
+	"github.com/bartam1/mobilfox/shorter/pkg/errors/httperror"
+	echo "github.com/labstack/echo/v4"
+)
+
+type Shorter struct {
+	service port.Service
+}
+
+func (s Shorter) GetUrlsWidthHash(ctx echo.Context) error {
+	urls, err := s.service.Queries.UrlsWidthHash.Do(ctx.Request().Context())
+	if err != nil {
+		httperror.InternalError(ctx, err)
+		return err
+	}
+	return ctx.JSON(http.StatusOK, urls)
+
+}
+func (s Shorter) GetUrl(ctx echo.Context, hash string) error {
+	url, err := s.service.Queries.Url.Do(ctx.Request().Context(), hash)
+	if err != nil {
+		httperror.InternalError(ctx, err)
+		return err
+	}
+	return ctx.JSON(http.StatusOK, url)
+}
+func (s Shorter) MakeUrlHash(ctx echo.Context) error {
+	u := domain.MakeUrlHash{Url: ctx.Request().PostFormValue("Url")}
+	url, err := s.service.Commands.MakeUrlHash.Do(ctx.Request().Context(), u)
+	if err != nil {
+		httperror.InternalError(ctx, err)
+		return err
+	}
+	return ctx.JSON(http.StatusOK, url)
+}
+func (s Shorter) DeleteUrl(ctx echo.Context, hash string) error {
+	err := s.service.Commands.DeleteUrl.Do(ctx.Request().Context(), hash)
+	if err != nil {
+		httperror.InternalError(ctx, err)
+		return err
+	}
+	return ctx.String(http.StatusOK, "Deleted")
+}
