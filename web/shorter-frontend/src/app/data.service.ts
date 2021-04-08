09a8af304b4386503c23b0f974917eef95b2b521
Marton Barta <bbartam@gmail.com>
2021-04-08 16:26:10 +0200

    added http error handling, get,post,remove func

--- web/shorter-frontend/src/app/data.service.ts
+++ web/shorter-frontend/src/app/data.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpErrorResponse } from '@angular/common/http';
+
+import {  throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +12,32 @@ export class DataService {
   private API_SERVER = "http://localhost:3000";
 
   constructor(private httpClient: HttpClient) { }
+  
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+  
   public GetUrlsWidthHash(){
     let url = this.API_SERVER + "/urls";
-    return this.httpClient.get(url);
+    return this.httpClient.get(url).pipe(retry(3),catchError(this.handleError));
   }
-  // public PostMakeUrlHash(){
-  //   let url = this.API_SERVER + "/url/new";
-  //   return this.httpClient.post(url);
-  // }
+  public PostMakeUrlHash(mu: string){
+    let url = this.API_SERVER + "/urls/new";
+    let reqBody = "Url=" + mu;
+    console.log(reqBody);
+    return this.httpClient.post(url,{'Url': mu}).pipe(catchError(this.handleError));
+  }
+  public RemoveUrl(u: string) {
+    let url = this.API_SERVER + "/url/del/" + u;
+    return this.httpClient.delete(url).pipe(catchError(this.handleError));
+  }
 }
