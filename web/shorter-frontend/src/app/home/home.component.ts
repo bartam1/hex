c04107a4bf0c790cb2ea1f4db7133b6b67c15431
Marton Barta <bbartam@gmail.com>
2021-04-08 16:22:22 +0200

    added remove url and unsubscribe when component destroyed

--- web/shorter-frontend/src/app/home/home.component.ts
+++ web/shorter-frontend/src/app/home/home.component.ts
@@ -1,21 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../data.service';
+import {  takeUntil } from 'rxjs/operators'
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit , OnDestroy {
+  
   UrlsWidthHash = []
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
-    this.dataService.GetUrlsWidthHash().subscribe((data: any[])=>{
+    this.dataService.GetUrlsWidthHash().pipe(takeUntil(this.destroy$)).subscribe((data: any[])=>{
       console.log(data);
       this.UrlsWidthHash = data;
     })  
   }
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 
+  removeUrl(u: string) {
+    this.dataService.RemoveUrl(u).subscribe((data: any[])=>{
+      console.log(data);
+  
+    })
+  }
 }
